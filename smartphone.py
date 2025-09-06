86ecabc1153546593374243f607a60ef7cc901bb
j-mareh <jacob.hirbo@students.uonbi.ac.ke>
2025-09-06 16:27:37 +0300

    smartphone.py

--- smartphone.py
+++ smartphone.py
@@ -35,3 +35,32 @@ phone = Smartphone("Samsung", "Galaxy S24", "256GB", 80)
 phone.make_call("0712345678")
 phone.charge()
 phone.run_diagnostics()
+
+
+
+
+class Vehicle:
+    def move(self):
+        print("This vehicle moves in some way...")
+
+class Car(Vehicle):
+    def move(self):
+        print("🚗 Driving on the road")
+
+class Plane(Vehicle):
+    def move(self):
+        print("✈️ Flying in the sky")
+
+class Boat(Vehicle):
+    def move(self):
+        print("⛵ Sailing on the water")
+
+
+# Polymorphism in action
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()   # Same method name, different behavior
+
+
+
